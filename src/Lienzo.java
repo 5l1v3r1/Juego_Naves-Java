ad5a5b5d5b29a78f06bf01b9c136f767bb9fcfb1
SebastiánEPH <sebastianeph99@gmail.com>
2020-08-11 19:35:28 -0500

    new files in Intellj

---
+++ src/Lienzo.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Lienzo extends JFrame {
+    // Variables de resolución de lienzo
+    public static final int WIDTH = 800, HEIGHT = 600;
+    public Lienzo(){
+        setTitle("Juego -  prueba ");
+
+    }
+
+
+
+    public static void main(String[] args){
+        System.out.println("Hola mundo en IntellJ");
+    }
+}
